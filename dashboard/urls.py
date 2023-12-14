92f75c3a93893de731c4c676418f1810d41d5c94
KhalidIbnNoghi <khalid.s39933@gmail.com>
2023-12-14 16:14:37 +0300

    Добавлена функция добавления категорий

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from dashboard.views import HomePageView
+from dashboard.views import HomePageView, CategoryAddView
 
 
 urlpatterns = [
     path("", HomePageView.as_view(), name='home'),
+    path("category_add/", CategoryAddView.as_view(), name='category_add'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,8 +1,8 @@
 from typing import Any
 from django.db.models import Sum
 from django.db.models.query import QuerySet
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, View
 from dashboard.models import Category
 
 class HomePageView(ListView):
@@ -10,6 +10,18 @@ class HomePageView(ListView):
     context_object_name = 'categories'
 
     def get_queryset(self) -> QuerySet[Any]:
+        if self.request.user.is_anonymous:
+            return
         queryset = Category.objects.filter(author=self.request.user).prefetch_related(
             'payments').annotate(payment_sum=Sum('payments__sum'))
         return queryset
+    
+
+class CategoryAddView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'category_add.html')
+    def post(self, request, *args, **kwargs):
+        name = request.POST.get('category')
+        category = Category(name=name, author=request.user)
+        category.save()
+        return redirect('home')
