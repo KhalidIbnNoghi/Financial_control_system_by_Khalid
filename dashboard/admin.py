829564f1818cb46f194c53b2e55de30c75317c48
KhalidIbnNoghi <khalid.s39933@gmail.com>
2023-12-21 20:22:47 +0300

    Добавлены фильтры в админку

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
-from dashboard.models import Category, Payment
+from dashboard.models import Category, Payment, Profile
 
-admin.site.register(Category)
-admin.site.register(Payment)
+
+# admin.site.register(Category)
+# admin.site.register(Payment)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    fields = ( 'name', 'author' )
+    list_display = ( 'name', 'author' )
+    list_filter = ( 'name', 'author' )
+    ordering = ('-name',)
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    fields = ( 'sum', 'date', 'category' )
+    list_display = ( 'sum', 'date', 'category' )
+    list_filter = ( 'sum', 'date' )
+    ordering = ('-date',)
